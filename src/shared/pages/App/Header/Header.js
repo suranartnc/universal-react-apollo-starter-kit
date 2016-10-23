a55c5836af8ca4ba60d49af1186a8c4a6b8127f1
SuranartN <suranartnc@gmail.com>
2016-10-23 14:50:01 +0700

    add header, navigation

--- src/shared/pages/App/Header/Header.js
+++ src/shared/pages/App/Header/Header.js
@@ -1,42 +1,26 @@
 import React, { Component } from 'react'
-import cx from 'classnames'
 import { Link } from 'react-router'
-
-import SearchBar from './SearchBar/SearchBar'
-import MemberMenu from './MemberMenu/MemberMenu'
 
 import styles from './Header.scss'
 
 class Header extends Component {
-
-  state = {
-    isMenuOpen: false,
-  }
-
-  toggleMenu = () => {
-    this.setState({
-      isMenuOpen: !this.state.isMenuOpen,
-    })
-  }
-
   render() {
     return (
       <header className={styles.container}>
         <div className={styles.wrapper}>
-          <button onClick={this.toggleMenu} className={styles['menu-button']}>
-            <div className={cx(styles['menu-button-icon'], this.state.isMenuOpen)}>
+          <button onClick={this.toggleMenu}>
+            <div>
               <span />
               <span />
               <span />
             </div>
           </button>
           <div className={styles.logo}>
-            <Link to="/"><h1 className={styles['logo-text']}>TRIPBY.ME</h1></Link>
+            <Link to="/"><h1 className={styles['logo-text']}>React Relay Starter Kit</h1></Link>
           </div>
           <div className={styles.panel}>
-            <SearchBar />
+
           </div>
-          <MemberMenu />
         </div>
       </header>
     )
