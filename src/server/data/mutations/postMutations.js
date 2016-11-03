584cf5dd0a8fc84df3065683fd8ae0a7dde0ff0b
Suranart Niamcome <suranartnc@gmail.com>
2016-11-03 10:23:17 +0700

    make schema with no relay compat works

--- src/server/data/mutations/postMutations.js
+++ src/server/data/mutations/postMutations.js
@@ -5,46 +5,21 @@ import {
 } from 'graphql'
 
 import PostModel from '../models/PostModel'
-
-import userType from '../types/userType'
-
+import postType from '../types/postType'
 import { outputError } from '../utils/helpers'
 
-export const addPostMutation = mutationWithClientMutationId({
-  name: 'AddPost',
-  inputFields: {
+export const addPostMutation = {
+  type: postType,
+  description: 'Create a new post',
+  args: {
     title: { type: new GraphQLNonNull(GraphQLString) },
     body: { type: new GraphQLNonNull(GraphQLString) },
     excerpt: { type: GraphQLString },
     categories: { type: new GraphQLList(GraphQLString), description: 'Id of categories' },
     userId: { type: new GraphQLNonNull(GraphQLString), description: 'Id of the author' },
   },
-  outputFields: {
-    postEdge: {
-      type: postConnection.edgeType,
-      resolve: postItem => PostModel.find()
-        .then((allPosts) => {
-          let postToFindIndex
-          for (const post of allPosts) {
-            if (post._id.toString() === postItem._id.toString()) {
-              postToFindIndex = post
-            }
-          }
-          const cursorId = cursorForObjectInConnection(allPosts, postToFindIndex)
-          return {
-            node: postItem,
-            cursor: cursorId,
-          }
-        }),
-    },
-    viewer: {
-      type: userType,
-      resolve: () => {
-        return {
-          _id: '5805c26198f0370001ac64a3', // example user
-        }
-      },
-    },
+  resolve: (source, args) => {
+    const post = Object.assign({}, args)
+    return PostModel.create(post).catch(error => outputError(error))
   },
-  mutateAndGetPayload: post => PostModel.create(post).catch(error => outputError(error)),
-})
+}
