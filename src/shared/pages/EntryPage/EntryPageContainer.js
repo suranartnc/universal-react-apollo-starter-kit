8aae671d32e81670612b072734aecdd4f1d33226
Suranart Niamcome <suranartnc@gmail.com>
2016-11-04 10:25:39 +0700

    query the static post on entrry page

--- src/shared/pages/EntryPage/EntryPageContainer.js
+++ src/shared/pages/EntryPage/EntryPageContainer.js
@@ -1,20 +1,39 @@
 import React, { Component, PropTypes } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
 import EntryPage from './EntryPage'
 
 class EntryPageContainer extends Component {
   render() {
-    return (
-      <EntryPage post={this.props.post} />
-    )
+    if (!this.props.data.loading) {
+      return (
+        <EntryPage post={this.props.data.viewer.post} />
+      )
+    }
+    return <div>Loading...</div>
   }
 }
 
 EntryPageContainer.propTypes = {
   post: PropTypes.shape({
-    id: PropTypes.string,
+    _id: PropTypes.string,
     title: PropTypes.string,
     body: PropTypes.string,
   }),
 }
 
-export default EntryPageContainer
+
+const GET_POST = gql`
+  query getPost {
+    viewer {
+      post(_id: "5805a2d298f0370001ac64a2") {
+        _id
+        title
+        body
+      }
+    }
+  }
+`
+
+export default graphql(GET_POST)(EntryPageContainer)
