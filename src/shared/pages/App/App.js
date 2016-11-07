aea57f9e9e640349ae448925c5c6cc516b7115b5
TaToonz <peerapath.t@gmail.com>
2016-11-07 16:55:30 +0700

    bring back helmet

--- src/shared/pages/App/App.js
+++ src/shared/pages/App/App.js
@@ -1,11 +1,11 @@
 import React, { PropTypes } from 'react'
 
-//import Helmet from 'react-helmet'
+import Helmet from 'react-helmet'
 
 function App(props) {
   return (
     <div>
-      {/*<Helmet title="Universal React GraphQL Starter Kit" />*/}
+      <Helmet title="Universal React GraphQL Starter Kit" />
       { props.children }
     </div>
   )
