cc58c978b661ec0c7e0543b04fdee19ff4c0d2db
TaToonz <peerapath.t@gmail.com>
2016-11-10 15:45:09 +0700

    mock more users

--- src/server/data/models/UserModel.js
+++ src/server/data/models/UserModel.js
@@ -1,18 +1,31 @@
 import mongoose from 'mongoose'
 
+const userType = ['admin', 'subscriber']
+
 const UserSchema = new mongoose.Schema({
-  type: String,
+  type: {
+    type: String,
+    enum: userType,
+    default: 'subscriber',
+  },
   displayName: {
     type: String,
     unique: true,
+    required: true,
   },
   email: {
     type: String,
     unique: true,
+    required: true,
   },
-  avatar: String,
-  oauthID: Number,
-  oauthStrategy: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: 'https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg',
+  },
   date: {
     type: Date,
     default: Date.now,
