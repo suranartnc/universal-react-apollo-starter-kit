90431b6ad0e5409809c0b5123a9ad075c231f233
SuranartN <suranartnc@gmail.com>
2016-11-12 13:49:31 +0700

    add passport

--- src/server/data/models/UserModel.js
+++ src/server/data/models/UserModel.js
@@ -3,33 +3,32 @@ import mongoose from 'mongoose'
 const userType = ['admin', 'subscriber']
 
 const UserSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    enum: userType,
-    default: 'subscriber',
+  email: { type: String, unique: true },
+  password: String,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+
+  facebook: String,
+  twitter: String,
+  tokens: Array,
+
+  profile: {
+    type: {
+      type: String,
+      enum: userType,
+      default: 'subscriber',
+    },
+    displayName: String,
+    picture: {
+      type: String,
+      default: 'https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg',
+    },
   },
-  displayName: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    default: 'https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg',
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-})
+
+  // date: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+}, { timestamps: true })
 
 export default mongoose.model('User', UserSchema)
