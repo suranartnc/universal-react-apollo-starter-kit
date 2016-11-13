5cc4af3ff762a846f325b5da21ec57514a3fe463
SuranartN <suranartnc@gmail.com>
2016-11-13 16:44:17 +0700

    implement react-router-redux

--- src/shared/store/createStore.js
+++ src/shared/store/createStore.js
@@ -1,7 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from 'shared/reducers'
+import { routerMiddleware } from 'react-router-redux'
 
-export default (client, initialState) => {
-  const store = createStore(rootReducer(client), initialState)
+export default (client, history, initialState) => {
+  const middlewares = [
+    routerMiddleware(history),
+    client.middleware(),
+  ]
+
+  let enhancer = applyMiddleware(...middlewares)
+
+  if (process.env.BROWSER && process.env.NODE_ENV === 'development' && window.devToolsExtension) {
+    enhancer = compose(enhancer, window.devToolsExtension())
+  }
+
+  const store = createStore(rootReducer(client), initialState, enhancer)
   return store
 }
