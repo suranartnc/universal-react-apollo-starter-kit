67747e1b2a4b8e34a037caa15ba2c431a07a89df
SuranartN <suranartnc@gmail.com>
2016-11-20 20:34:15 +0700

    add bootstrap4 using react-helmet

--- src/shared/pages/App/App.js
+++ src/shared/pages/App/App.js
@@ -1,8 +1,18 @@
 import React, { PropTypes } from 'react'
+import Helmet from 'react-helmet'
 
 function App(props) {
   return (
     <div>
+      <Helmet
+        title="Universal React GraphQL Starter Kit"
+        meta={[
+            { name: 'description', content: 'A starter kit to bootstrap a universal web app using react, redux and apollo.' },
+        ]}
+        link={[
+            { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css' },
+        ]}
+      />
       { props.children }
     </div>
   )
