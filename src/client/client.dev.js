83f5144c894fa90572da0e23f5c2b546b83294de
SuranartN <suranartnc@gmail.com>
2016-11-20 15:08:56 +0700

    remove AppContainer

--- src/client/client.dev.js
+++ src/client/client.dev.js
@@ -1,27 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-
 import Root from 'shared/Root'
 
 const mountNode = document.getElementById('root')
 
-render(
-  <AppContainer>
-    <Root />
-  </AppContainer>,
-  mountNode
-)
-
-if (module.hot) {
-  module.hot.accept('shared/Root', () => {
-    const NextRootApp = require('shared/Root').default
-
-    render(
-      <AppContainer>
-        <NextRootApp />
-      </AppContainer>,
-      mountNode
-    )
-  })
-}
+render(<Root />, mountNode)
